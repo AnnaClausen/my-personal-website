d05805344ff14ccd74e5e3cc19f239023ccb792f
Anna Clausen <anna.revall.clausen@gmail.com>
2022-05-06 10:34:27 -0700

    Change Javascript event listener

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// Get the button, and when the user clicks on it, execute myFunction
-document.getElementById("myBtn").onclick = function() {myFunction()};
-
-/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+const element = document.getElementById("myBtn");
+element.addEventListener("click", function() {
+  document.getElementById("message").innerHTML = "Coming Soon!";
+});
